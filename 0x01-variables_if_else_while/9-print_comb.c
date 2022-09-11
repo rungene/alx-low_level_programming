195bafe17d7051f0288aeb6a92c32c7aa70a2ecb
rungene <rungenesamuel@gmail.com>
2022-09-11 11:57:58 -0700

    Write a program that prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry of the preogram
+ *
+ * Return: function returns zero on success
+ */
+int main (void)
+{
+	int ch;
+
+	for (ch =  0 + '0'; ch <=9 + '0'; ch++)
+	{
+	putchar(ch);
+	if(ch != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
