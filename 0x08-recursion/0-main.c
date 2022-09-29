695fa3edbd1be20fddd97c9490041dd5675b5278
rungene <rungenesamuel@gmail.com>
2022-09-29 10:04:03 -0700

    Write a function that prints a string, followed by a new line.

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* main - check the code
+* Return: Always
+*/
+int main(void)
+{
+	_puts_recursion("Puts with recursion");
+	return (0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+0-puts_recursion.c - Write a function that prints a string, followed by a new line.
+	Prototype: void _puts_recursion(char *s);
+
