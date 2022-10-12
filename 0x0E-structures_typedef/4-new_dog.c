febc04593f1c331ef9ec07adffea61be8d082a47
rungene <rungenesamuel@gmail.com>
2022-10-12 04:30:59 -0700

    initial commits

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* new_dog - creates a new dog structure
+* @name: Dog name
+* @age: Dog age
+* @owner: Dog Owner
+* Return: returns a pointer to newly created dog structure
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
