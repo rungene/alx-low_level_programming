895267e25f5b4b8c0dd60698aa0d04212586f7ae
rungene <rungenesamuel@gmail.com>
2023-01-17 10:23:02 -0800

    wqa function that allocates memory using malloc.

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* malloc_checked - a function that allocates memory using malloc.
+* @b:unsigned int parameter
+*
+* Return: Void pointer to the allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{	
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if(ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+		return ptr;
+}
