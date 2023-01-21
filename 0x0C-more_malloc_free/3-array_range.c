5f05724f850a5f62639c5ffb74dd6ff8f706b1af
rungene <rungenesamuel@gmail.com>
2023-01-21 00:43:21 -0800

    Corrections - betty errors

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -8,19 +8,19 @@
 */
 int *array_range(int min, int max)
 {
-	int i,numele;
+	int i, numele;
 	int *p;
-	
+
 	numele = 0;
 	if (min > max)
 		return (NULL);
-	for (i=min; i<= max; i++)
+	for (i = min; i <= max; i++)
 		numele += 1;
 	p = malloc(sizeof(int) * (numele));
 
 	if (p == NULL)
 		return (NULL);
 	for (i = min; i <= max; i++)
-		*(p+i) = min++;
+		*(p + i) = min++;
 	return (p);
 }
