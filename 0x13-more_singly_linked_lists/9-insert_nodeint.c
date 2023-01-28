08b820a6e5840d79b1e49a1eed42d295543e2164
rungene <rungenesamuel@gmail.com>
2023-01-27 20:13:28 -0800

    if the pointer is null , return null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp2 = *head;
 	/*empty list*/
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	/*create the new node to add*/
