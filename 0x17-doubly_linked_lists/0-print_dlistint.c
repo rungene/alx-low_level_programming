c90cb80b35505601cd05e9a186115d9a66d09940
rungene <rungenesamuel@gmail.com>
2023-02-05 08:12:14 -0800

    Fixing betty errors

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,7 +3,7 @@
 
 /**
 * print_dlistint - prints all the elements of a dlistint_t list
-* @h - head pointer to the doubly linked lis
+* @h: head pointer to the doubly linked lis
 *
 * Return: the number of nodes
 */
@@ -14,11 +14,11 @@ size_t print_dlistint(const dlistint_t *h)
 
 	temp = h;
 	num_nodes = 0;
-	while(temp != NULL)
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		num_nodes++;
 		temp = temp->next;
 	}
-	return num_nodes;
+	return (num_nodes);
 }
