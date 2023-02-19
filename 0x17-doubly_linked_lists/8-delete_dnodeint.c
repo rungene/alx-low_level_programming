0b6c29c630ca6ba43e4199090060c04891b3a4d0
rungene <rungenesamuel@gmail.com>
2023-02-19 03:50:22 -0800

    Adding a test to make sure wwe do not derefrence null

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,6 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)
 	{
 		*head = (*head)->next;
+		if (*head != NULL)
+			free((*head)->prev);
 		return (1);
 	}
 	while (index != 0)
