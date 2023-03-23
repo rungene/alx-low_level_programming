dd6b69cdf22056af39b4f2576bd07211c0b0605b
rungene <rungenesamuel@gmail.com>
2023-03-22 19:48:21 -0700

    Adding 5-island_perimetr.py module and adding documentation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter module
+"""
+
+
+def island_perimeter(grid):
+    """
+    island_perimeter calculates pereimeter 
+    of the island.
+
+    parameter:
+        grid (array): An 0's and 1's array that represents an island 
+        sourrounded by land(1)
+        sourrounded by water (0)
+
+    return:
+        perimeter of the island described in grid
+    """
+    perimeter = 0
+    if grid == 0:
+        water_zone = 0
+        perimeter = peremiter + (2 * water_zone)
+    elif grid == 1:
+        land_zone = 1
+        perimeter = perimeter + (2 * land_zone)
+           
+    # when completely srounded by water oneisland
+
